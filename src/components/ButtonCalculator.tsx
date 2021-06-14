f2fd58d6467d85c670b104c394240091804c2be5
“Chris <cvillarroel@bice.cl>
2021-06-14 13:25:20 -0400

    Added all buttons to UI

--- src/components/ButtonCalculator.tsx
+++ src/components/ButtonCalculator.tsx
@@ -1,21 +1,31 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 import { styles } from '../theme/appTheme'
 
 interface ButtonCalculatorProps {
-    text: string,
-    color?: string
+  text: string,
+  color?: string,
+  ancho?: boolean
 }
 
-export const ButtonCalculator = ({ text, color } : ButtonCalculatorProps) => {
-    return (
-        <View style={{
-            ...styles.btn,
-            backgroundColor: color
-        }}>
-            <Text style={styles.btnText}>{text}</Text>
-        </View>
-    )
+export const ButtonCalculator = ({ text, color = '#2D2D2D', ancho = false }: ButtonCalculatorProps) => {
+  return (
+    <TouchableOpacity>
+      <View style={{
+        ...styles.btn,
+        backgroundColor: color,
+        width: (ancho) ? 180 : 80
+      }}>
+        <Text
+          style={{
+            ...styles.btnText,
+            color: (color === '#9B9B9B') ? 'black' : 'white'
+          }}
+
+        >{text}</Text>
+      </View>
+    </TouchableOpacity>
+  )
 }
 
 export default ButtonCalculator
--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -6,18 +6,42 @@ import { styles } from '../theme/appTheme'
 export const CalculatorScreen = () => {
   return (
     <View style={styles.calculatorContainer}>
-        <Text style={styles.smallResult}>1.500.000</Text>
-        <Text style={styles.result}>1.500.000</Text>
+      <Text style={styles.smallResult}>1.500.000</Text>
+      <Text style={styles.result}>1.500.000</Text>
 
-        <View style={styles.row}>
+      <View style={styles.row}>
+        <ButtonCalculator text="C" color="#9B9B9B" />
+        <ButtonCalculator text="+/-" color="#9B9B9B" />
+        <ButtonCalculator text="del" color="#9B9B9B" />
+        <ButtonCalculator text="/" color="#FF9427" />
+      </View>
 
-          {/* Boton */}
-          <ButtonCalculator text="C" color="#9B9B9B" />
-          <ButtonCalculator text="+/-" color="#9B9B9B" />
-          <ButtonCalculator text="del" color="#9B9B9B" />
-          <ButtonCalculator text="/" color="#FF9427" />
+      <View style={styles.row}>
+        <ButtonCalculator text="7" />
+        <ButtonCalculator text="8" />
+        <ButtonCalculator text="9" />
+        <ButtonCalculator text="X" color="#FF9427" />
+      </View>
 
-        </View>
+      <View style={styles.row}>
+        <ButtonCalculator text="4" />
+        <ButtonCalculator text="5" />
+        <ButtonCalculator text="6" />
+        <ButtonCalculator text="-" color="#FF9427" />
+      </View>
+
+      <View style={styles.row}>
+        <ButtonCalculator text="1" />
+        <ButtonCalculator text="2" />
+        <ButtonCalculator text="3" />
+        <ButtonCalculator text="+" color="#FF9427" />
+      </View>
+
+      <View style={styles.row}>
+        <ButtonCalculator text="0" ancho={true} />
+        <ButtonCalculator text="." />
+        <ButtonCalculator text="=" color="#FF9427" />
+      </View>
     </View>
   )
 }
